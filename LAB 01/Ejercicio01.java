0ace3e764483db2482551a936e292e11dcb277e6
TorresJhair <jtorreschav@unsa.edu.pe>
2023-09-21 12:36:46 -0500

    Ejercicio 01 añadido

---
+++ LAB 01/Ejercicio01.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Ejercicio01{
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+       
+        String sol1 = sc.next();
+        String sol2 = sc.next();
+        String sol3 = sc.next();
+        String sol4 = sc.next();
+        String sol5 = sc.next();
+        System.out.println("Ej;ército:");
+        showSold(sol1, sol2, sol3, sol4, sol5);
+    }
+    public static void showSold (String s1, String s2, String s3, String s4, String s5){
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+    }
+}
