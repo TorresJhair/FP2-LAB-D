1988d970e937c98f346d56d5dfe5c98831420f23
TorresJhair <jtorreschav@unsa.edu.pe>
2023-09-21 14:26:37 -0500

    Ejercicio 02 - Clase Soldado: Añardido

---
+++ LAB 03/Ejercicio02.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Ejercicio02{
+    public static void main (String [] args){
+        Soldado [] ejercito = new Soldado[5];
+        ingresarSoldados(ejercito);
+        for(int i = 0; i < ejercito.length; i++){
+            System.out.println(ejercito[i]);
+        }
+        mostrarEjercito(ejercito);
+    }
+    public static void ingresarSoldados(Soldado [] ejercito){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < ejercito.length; i++){
+            System.out.println("Soldado " + (i + 1));
+            System.out.println("Nombre:");
+            String name = sc.next();
+            System.out.println("Vida:");
+            int vida = sc.nextInt();
+            ejercito[i] = new Soldado(name, vida);
+        }
+    }
+    public static void mostrarEjercito(Soldado [] ejercito){
+        System.out.println("Ejército:");
+        for(int i = 0; i < ejercito.length; i++){
+            System.out.println(ejercito[i]);
+        }
+    }
+}
