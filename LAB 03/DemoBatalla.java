005b053bc756b6a84efd95c5fa0d427f293448b1
TorresJhair <jtorreschav@unsa.edu.pe>
2023-09-21 14:12:37 -0500

    LAB 03 añadido

---
+++ LAB 03/DemoBatalla.java
@@ -0,0 +1,89 @@
+import java.util.*;
+public class DemoBatalla {
+    public static void main(String [] args){
+        Nave [] misNaves = new Nave[10];
+        Scanner sc = new Scanner(System.in);
+        String nomb, col;
+        int fil, punt;
+        boolean est;
+        for (int i = 0; i < misNaves.length; i++) {
+            System.out.println("Nave " + (i + 1));
+            System.out.print("Nombre: ");
+            nomb = sc.next();
+            System.out.println("Fila ");
+            fil = sc.nextInt();
+            System.out.print("Columna: ");
+            col = sc.next();
+            System.out.print("Estado: ");
+            est = sc.nextBoolean();
+            System.out.print("Puntos: ");
+            punt = sc.nextInt();
+            misNaves[i] = new Nave(nomb, fil, col, est, punt); 
+            System.out.println("\nNaves creadas:");
+            mostrarNaves(misNaves);
+            mostrarPorNombre(misNaves);
+            mostrarPorPuntos(misNaves);
+            System.out.println("\nNave con mayor número de puntos: " + mostrarMayorPuntos(misNaves));
+            System.out.println("Nuevo orden de la flota:");
+            System.out.println(desordenarNaves(misNaves));
+        }
+    }
+    //Método para mostrar todas las naves
+    public static void mostrarNaves(Nave [] flota){
+        for(int i = 0; i < flota.length; i++){
+            System.out.println(flota[i]);
+        }
+    }
+    //Método para mostrar todas las naves de un nombre que se pide por teclado
+    public static void mostrarPorNombre(Nave [] flota){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Nombre a buscar:");
+        String nombr = sc.next();
+        for(int i = 0; i < flota.length; i++){
+            if(flota[i].getNombre().equals(nombr)){
+                System.out.println(flota[i]);
+            }
+        }
+    }
+    //Método para mostrar todas las naves con un número de puntos inferior o igual
+    //al número de puntos que se pide por teclado
+    public static void mostrarPorPuntos(Nave [] flota){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cantidad de puntos:");
+        int puntos = sc.nextInt();
+        for(int i = 0; i < flota.length; i++){
+            if(flota[i].getPuntos() <= puntos){
+                System.out.println(flota[i]);
+            }
+        }
+    }
+    //Método que devuelve la Nave con mayor número de Puntos
+    public static Nave mostrarMayorPuntos(Nave [] flota){
+        int max = flota[0].getPuntos();
+        for(int i = 0; i < flota.length; i++){
+            max = Math.max(max, flota[i].getPuntos());
+        }
+        for(int i = 0; i < flota.length; i++){
+            if(flota[i].getPuntos() == max){
+                return flota[i];
+            }
+        }
+        return flota[0];
+    }
+    //Crear un método que devuelva un nuevo arreglo de objetos con todos los objetos previamente ingresados
+    //pero aleatoriamente desordenados
+    public static Nave [] desordenarNaves(Nave [] flota){
+        Nave [] nuevaFlota = new Nave[flota.length];
+        int posicion;
+        for(int i = 0; i < flota.length; i++){
+            posicion = (int)(Math.random() * flota.length);
+            //Si la posicion NO se encuentra ocupada
+            for(int j = 0; j < nuevaFlota.length; j++){
+                if (! flota[i].getNombre().equals(nuevaFlota[j].getNombre())){
+                    nuevaFlota[posicion] = flota[i];
+                }
+            }
+        }
+        return nuevaFlota;
+    }
+}
